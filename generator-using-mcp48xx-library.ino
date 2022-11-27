cbdcd2ad5d5087da4334c6dbf6a9b6a0b2e056a0
ojacomarket <68973718+ojacomarket@users.noreply.github.com>
2022-11-27 23:55:34 +0200

    Update generator-using-mcp48xx-library.ino

--- generator-using-mcp48xx-library.ino
+++ generator-using-mcp48xx-library.ino
@@ -13,46 +13,21 @@
 #define TRIANGLE 1
 #define SINE 0
 
-unsigned long start = 0;
-unsigned long finish = 0;
 byte waveform = SINE;
 MCP4802 mcp4802(CS_PIN);
 
 void setup() {
   digitalWrite(LDAC_PIN, LOW); // only if LDAC is LOW our MCP4802 will work
   Serial.begin(9600);
-  mcp4802.init();
-  mcp4802.setGainA(MCP4802::High);
-  mcp4802.turnOnChannelA();
-  mcp4802.setVoltageA(0);
-  mcp4802.updateDAC();
+  mcp4802.init(); // must have
+  mcp4802.setGainA(MCP4802::High); // Output voltage is 4.08 V always!
+  mcp4802.turnOnChannelA(); // Channel A is 4.08 V, channel B is 0 V
+  mcp4802.setVoltageA(0); // Clear voltage is was there
+  mcp4802.updateDAC(); // Send signal to DAC
   pinMode(D8, INPUT);    // sets the digital pin 7 as input
   pinMode(D7, INPUT);    // sets the digital pin 6 as input
   pinMode(D6, INPUT);    // sets the digital pin 5 as input
   pinMode(D5, INPUT);    // sets the digital pin 4 as input
-  // Set Timer 1 to normal mode at F_CPU.
-  TCCR1A = 0;
-  TCCR1B = 1;
-
-  // Time digitalWrite().
-  cli();
-  uint16_t start = TCNT1;
-  for (unsigned int i = 0; i < DAC_RESOLUTION; i++)
-  {
-    waveform = choose_waveform(waveform);
-    mcp4802.setVoltageA((unsigned int)WAVEFORMS[waveform][i]);
-    mcp4802.updateDAC();
-    //delayMicroseconds(map(analogRead(1), 512, 1023, 0, 5000));
-    // delay(map(analogRead(1), 512, 1023, 0, 500));
-    //Serial.println(analogRead(A0));
-  }
-  uint16_t finish = TCNT1;
-  sei();
-  uint16_t overhead = 8;
-  uint16_t cycles = finish - start - overhead;
-  Serial.print("digitalWrite() took ");
-  Serial.print(cycles);
-  Serial.println(" CPU cycles.");
 }
 
 void loop()
@@ -62,9 +37,8 @@ void loop()
     waveform = choose_waveform(waveform);
     mcp4802.setVoltageA((unsigned int)WAVEFORMS[waveform][i]);
     mcp4802.updateDAC();
-    //delayMicroseconds(map(analogRead(1), 512, 1023, 0, 5000));
-    // delay(map(analogRead(1), 512, 1023, 0, 500));
-    //Serial.println(analogRead(A0));
+    delayMicroseconds(map(analogRead(1), 512, 1023, 0, 5000)); // frequency control
+    // Serial.println(analogRead(A0));
   }
 }
 
