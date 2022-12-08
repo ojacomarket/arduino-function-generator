578f086fad1d0d898ea9b21a428e9ca6189d8b6c
ojacomarket <68973718+ojacomarket@users.noreply.github.com>
2022-12-09 01:18:17 +0200

    Update generator-using-direct-port-writing.ino
    pay attention to second SPDR shift!

--- generator-using-direct-port-writing.ino
+++ generator-using-direct-port-writing.ino
@@ -1,3 +1,4 @@
+
 #include <Waveforms8.h>
 #define DAC_RESOLUTION 256 // MCP4802 has 8 bits resolution
 #define SQUARE 3
@@ -7,26 +8,27 @@
 
 const byte MCP4802_REGISTER  = 0b0001; // first bit (channel A or B --> 0 or 1), second bit (always 0), third bit (gain of 1 or 2, 1 or 0), fourth bit (always 1)
 byte counter = 0;
+unsigned int val = 0;
 volatile byte mcp_register = 0;
 void setup() {
+  Serial.begin(9600);
   DDRB |= (1 << DDB5) | (1 << DDB3) | (1 << DDB2); // SPI.begin();
   PORTB |= (1 << PORTB2);
   SPCR = 0b01010000;//(1 << SPE) | (1 << MSTR) | (0 << CPOL) | (0 << CPHA) | (8 << SPR0);
 }
 
 void loop() {
-  mcp_register = WAVEE[SINE][counter];
+  mcp_register = WAVEFORMS[SINE][counter];
   PORTB &= ~(1 << PORTB2);
   SPDR = (MCP4802_REGISTER << 4) | (mcp_register >> 4);
   while (!(SPSR & (1 << SPIF)));
-  SPDR = (mcp_register >> 4);
+  SPDR = (mcp_register << 4); // dont mess ith >>4
   while (!(SPSR & (1 << SPIF)));
   PORTB |= (1 << PORTB2);
-  counter = (counter + 1) % 64;
+  counter = (counter + 1) % 256;
   // change frequency on fly
-  /* val = analogRead(1);
-    if (val <= 530)
-     delayMicroseconds(0);
-    else
-     delayMicroseconds(val - 530);*/
+  val = analogRead(1);
+  if (val > 530)
+    delayMicroseconds(val - 530);
+  Serial.println(val);
 }
